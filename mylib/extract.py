be3d3fcb9cf5d27fd571d4794bf175b278240ed3
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-10-04 05:54:52 +0000

    changes

--- mylib/extract.py
+++ mylib/extract.py
@@ -3,15 +3,21 @@ Extract a dataset from a URL like Kaggle or data.gov. JSON or CSV formats tend t
 
 food dataset
 """
+import os
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/GroceryDB_IgFPro.csv"):
-    """"Extract a url to a file path"""
+
+def extract(
+    url="""
+    https://raw.githubusercontent.com/prasertcbs/basic-dataset/master/baskin_icecream.csv
+    """,
+    file_path="data/baskin_icecream.csv",
+    directory="data",
+):
+    """Extract a url to a file path"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     with requests.get(url) as r:
-        with open(file_path, 'wb') as f:
+        with open(file_path, "wb") as f:
             f.write(r.content)
     return file_path
-
-
-
