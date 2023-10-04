303f6ec06b553532ce00c9ff9aa9a2dba05fa9d2
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-10-04 06:25:50 +0000

    changes

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,26 +1,50 @@
 """
 Transforms and Loads data into the local SQLite3 database
-Example:
-,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
 """
 import sqlite3
 import csv
-import os
 
-#load the csv file and insert into a new sqlite3 database
-def load(dataset="/workspaces/sqlite-lab/data/GroceryDB_IgFPro.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
-    print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=''), delimiter=',')
-    conn = sqlite3.connect('GroceryDB.db')
+# load the csv file and insert into a new sqlite3 database
+def load(dataset="data/baskin_icecream.csv"):
+    """Transforms and Loads data into the local SQLite3 database"""
+    baskin_data = csv.reader(open(dataset, newline=""), delimiter=",")
+    # skips the header of csv
+    next(baskin_data)
+    conn = sqlite3.connect("baskin_icecream.db")
     c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
-    #insert
-    c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
+    c.execute("DROP TABLE IF EXISTS baskin_icecream")
+    c.execute(
+        """
+        CREATE TABLE baskin_icecream (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Flavour TEXT,
+            Calories INTEGER,
+            Total_Fat_g REAL,
+            Trans_Fat_g REAL,
+            Carbohydrates_g INTEGER,
+            Sugars_g INTEGER,
+            Protein_g REAL,
+            Size TEXT
+        )
+    """
+    )
+    # insert
+    c.executemany(
+        """
+        INSERT INTO baskin_icecream (
+            Flavour, 
+            Calories, 
+            Total_Fat_g, 
+            Trans_Fat_g, 
+            Carbohydrates_g, 
+            Sugars_g, 
+            Protein_g, 
+            Size
+            ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+        baskin_data,
+    )
     conn.commit()
     conn.close()
-    return "GroceryDB.db"
-
+    return "baskin_icecream.db"
